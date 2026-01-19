5b425fb8525eab19ae754e9094c3cb2bdba4e59c
switchz-glitch <switchz916@gmail.com>
2026-01-19 17:58:47 +0530

    Initial commit

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ b = 24;
 let temp = "let temp";
 
 const temp_const = "Hello World";
+
+var d;
+var d = 12;
+var name = "John";
+var name1 = "Doe";
+
+var temp1 = 12;
+temp1 = "new value";
+var temp2 = 24;
