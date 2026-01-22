7d983eff31c916ccf76abedfee4ce9be11ad1dbe
switchz-glitch <switchz916@gmail.com>
2026-01-22 19:40:50 +0530

    update

---
+++ loops.js
@@ -0,0 +1,56 @@
+// loops
+// 1 1 1 1 1 1 
+// 1 2 3 4 5 6
+
+// for loop
+// why use for loop? when we know the number of iterations
+// syntax:
+// for(initialization; condition; increment/decrement){
+//     // code to be executed
+// }
+
+
+// for (i = 0; i < 5; i++) {
+//     console.log("for loop iteration:", i);
+// }
+
+// for (let j = 9; j > 1; j--) {
+//     console.log("for loop iteration:", j);
+// }
+
+
+// for (let k = 0; k < 10; k++) {
+//     console.log("Hello");
+// }
+
+// while loop
+// kaya thi javanu chhe --> kaya rokava nu chhe --> kevi rite javanu chhe
+// syntax:
+// while(condition){
+//     // code to be executed
+//     // increment/decrement
+// }
+// let h = 5;
+// while (h <= 10) {
+//     console.log("while loop iteration:", h);
+//     h++;
+// }
+
+
+// let j = 50;
+// while (j <= 100) {
+//     console.log(j);
+//     j++;
+// }
+
+    // let a = 60;
+    // while (a <= 120) {
+    //     console.log(a);
+    //     a++;
+    // }
+
+let c = 30
+while (c >= 20) {
+    console.log("c:",c);
+    c--;
+}
